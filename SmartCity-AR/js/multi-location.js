35bf21e1816b082799566d0e475d8fcd2d0b716d
nbexultan <56743258+nbexultan@users.noreply.github.com>
2021-03-03 03:43:09 +0100

    outdoor

--- SmartCity-AR/js/multi-location.js
+++ SmartCity-AR/js/multi-location.js
@@ -35,7 +35,7 @@ var models = [
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '0.02 0.02 0.02',
+        scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
